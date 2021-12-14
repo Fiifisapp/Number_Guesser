e0df4886d245c4e0a9cda7400f00de387077f266
Fiifisapp <91861643+Fiifisapp@users.noreply.github.com>
2021-12-14 11:20:41 +0000

    Add files via upload

---
+++ Number Guesser/script.js
@@ -0,0 +1,33 @@
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
+
+// Write your code below:
+const generateTarget = () => {
+//this code generates a random number between 0 and 9
+    return Math.floor(Math.random()* 10);
+};
+
+const compareGuesses = (humanNum, computerNum, target) => {
+    let compuGuess = Math.abs(target - computerNum)
+    let humanGuess = Math.abs(target - humanNum)
+
+    if (humanNum > 9 || humanNum < 0)
+        alert("please enter a number between 0 and 9");
+    
+    if (humanGuess <= compuGuess)
+        return true;
+    else
+        return false;
+}
+
+const updateScore = (winner) => {
+    if (winner === "human")
+        humanScore ++;
+    else if (winner === "computer")
+        computerScore ++;
+}
+
+const advanceRound = () => {
+    currentRoundNumber ++;
+}
